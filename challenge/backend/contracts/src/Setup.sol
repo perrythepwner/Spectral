e03a6073fe80fa2a9191f3b20220e74acbde281c
simonedimaria <117766011+perrythepwner@users.noreply.github.com>
2025-05-21 10:10:30 +0200

    minor challenge source fixes

--- challenge/backend/contracts/src/Setup.sol
+++ challenge/backend/contracts/src/Setup.sol
@@ -1,24 +1,21 @@
 // SPDX-License-Identifier: MIT
-
 pragma solidity ^0.8.29;
 
 import { VCNK } from "./VCNK.sol";
 
 contract Setup {
     VCNK public TARGET;
-    address public player;
 
     event DeployedTarget(address at);
 
-    constructor(address _player) {
+    constructor() {
         TARGET = new VCNK();
-        player = _player;
         emit DeployedTarget(address(TARGET));
     }
 
     function isSolved() public view returns (bool) {
         uint8 CU_STATUS_EMERGENCY = 3;
-        (uint8 status, , ,) = TARGET.controlUnit();
+        (uint8 status, , , ) = TARGET.controlUnit();
         return status == CU_STATUS_EMERGENCY;
     }
 }
--- challenge/backend/contracts/src/VCNK.sol
+++ challenge/backend/contracts/src/VCNK.sol
@@ -7,7 +7,7 @@ pragma solidity ^0.8.29;
  | |/ / _ \/ / _ \/ _ `/ // / _ `/ / /__/ -_) _ \/ __/ __/ _ `/ / /_ // -_) _  / /    / // / __/ / -_) _ `/ __/ / ,< / -_) __/ _ \/ -_) / 
  |___/\___/_/_//_/\_,_/\_, /\_,_/  \___/\__/_//_/\__/_/  \_,_/_/_//__/\__/\_,_/ /_/|_/\_,_/\__/_/\__/\_,_/_/   /_/|_|\__/_/ /_//_/\__/_/  
                       /___/                                                                                                               
-                                          Volnaya Centralized Nuclear Kernel
+                                                      Volnaya Centralized Nuclear Kernel
 */
 
 interface vcnkCompatibleReceiver {
